a1a5c5c41ea330c5342432620aa21c08d8d5210c
Weiss Bai <whitesgr03@gmail.com>
2022-06-28 20:57:21 +0800

    Update Script.js, add getNumber Fn and getButton Fn

--- script.js
+++ script.js
@@ -15,13 +15,47 @@
 // }
 
 function operate(op, a, b) {
-
-    const methods = {
-        "-": (a, b) => a - b,
-        "+": (a, b) => a + b,
-        "x": (a, b) => a * b,
-        "÷": (a, b) => a / b,
+    const display = document.querySelector('.result').textContent
+    const isOperatorActive = document.querySelector('.active');
+    if (isOperatorActive) {
+        isOperatorActive.classList.remove('active')
     }
+    this.classList.add('active'); 
+    
 
-    return methods[op](a, b);
+    // const methods = {
+    //     "-": (a, b) => a - b,
+    //     "+": (a, b) => a + b,
+    //     "x": (a, b) => a * b,
+    //     "÷": (a, b) => a / b,
+    // }
+
+    // return methods[op](a, b);
 }
+
+function getNumber() {
+    const display = document.querySelector('.result')
+    display.textContent = this.textContent;
+}
+
+// function changeBGC() { 
+//     if (this.classList.contains('clicking')) {
+//         this.classList.remove('clicking')
+//     }
+//     this.classList.add('clicking')
+// }
+
+function getButtons() {
+    const numButtons = [...document.querySelectorAll('.num button')];
+    const operatorButton = [...document.querySelectorAll('.operator button')];
+
+    for (let button of numButtons) {
+        button.addEventListener('click', getNumber);
+        // button.addEventListener('mousedown mouseup', changeBGC) 滑鼠點擊變色事件還沒好
+    }
+    for (let button of operatorButton) {
+        button.addEventListener('click', operate);
+    }
+}
+
+getButtons();
