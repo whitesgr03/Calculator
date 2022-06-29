29027d80e4fde54e1cf3365e0dd93a4af82e2c24
Weiss Bai <whitesgr03@gmail.com>
2022-06-29 22:14:53 +0800

    Update script.js
    add rgbToHsl Fn and changeButtonBGC Fn listen for mouse events,
    increase or decrease the brightness of the button

--- script.js
+++ script.js
@@ -15,12 +15,16 @@
 // }
 
 function operate(op, a, b) {
-    const display = document.querySelector('.result').textContent
+    const fisrtNumber = document.querySelector('.result').textContent
+    
     const isOperatorActive = document.querySelector('.active');
+
     if (isOperatorActive) {
         isOperatorActive.classList.remove('active')
     }
-    this.classList.add('active'); 
+    if (this.textContent !== '=') {
+        this.classList.add('active'); 
+    }
     
 
     // const methods = {
@@ -38,22 +42,74 @@ function getNumber() {
     display.textContent = this.textContent;
 }
 
-// function changeBGC() { 
-//     if (this.classList.contains('clicking')) {
-//         this.classList.remove('clicking')
-//     }
-//     this.classList.add('clicking')
-// }
+function rgbToHsl(rgb){
+
+    let [r, g, b] = rgb.map(color => color / 255);
+
+    const max = Math.max(r, g, b);
+    const min = Math.min(r, g, b);
+
+    let h = 0;
+    let s = 0;
+    let l = (max + min) / 2;
+
+    if (max !== min) {
+        const d = max - min;
+        s = d / (1 - Math.abs(2 * l - 1));
+        switch(max){
+            case r:
+                h = (g - b) / d % 6;
+                break;
+            case g:
+                h = (b - r) / d + 2;
+                break;
+            case b:
+                h = (r - g) / d + 4;
+                break;
+        }
+    }
+
+    h = Math.round(h * 60);
+        
+    if (h < 0)
+        h += 360;
+
+    s = Math.round((s * 100));
+    l = Math.round((l * 100));
+
+    return [h, s, l];
+}
+
+function changeButtonBGC(e) {
+    if (e.type === 'mouseup') {
+        this.removeAttribute('style');
+        return
+    }
+
+    const rgb = window.getComputedStyle(this)['background-color'];
+    const rgbCode = rgb.match(/\d+/g, "");
+    let [Hue, Saturation, Lightness] = rgbToHsl(rgbCode);
+
+    if (this.parentNode.classList.contains('operator')) {
+        Lightness -= 15;
+    } else {
+        Lightness += 15
+    }
+
+    const newColor = `hsl(${Hue}, ${Saturation}%, ${Lightness}%)`;
+
+    this.style['background-color'] = newColor;
+}
 
 function getButtons() {
     const numButtons = [...document.querySelectorAll('.num button')];
-    const operatorButton = [...document.querySelectorAll('.operator button')];
+    const operatorButtons = [...document.querySelectorAll('.operator button')];
 
     for (let button of numButtons) {
         button.addEventListener('click', getNumber);
-        // button.addEventListener('mousedown mouseup', changeBGC) 滑鼠點擊變色事件還沒好
     }
-    for (let button of operatorButton) {
+
+    for (let button of operatorButtons) {
         button.addEventListener('click', operate);
     }
 }
